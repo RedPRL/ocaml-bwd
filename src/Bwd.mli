c3941af9bb63111beb4321cb466a9aa7f4f32068
favonia <favonia@gmail.com>
2022-04-29 13:30:40 -0500

    docs(Bwd): remove the "with type" from the interface

--- src/Bwd.mli
+++ src/Bwd.mli
@@ -2,6 +2,6 @@ type 'a bwd = 'a BwdDef.bwd =
   | Emp
   | Snoc of 'a bwd * 'a
 
-module Bwd : module type of BwdNoLabels with type 'a t = 'a bwd
-module BwdLabels : module type of BwdLabels with type 'a t = 'a bwd
+module Bwd : module type of BwdNoLabels
+module BwdLabels : module type of BwdLabels
 module BwdNotation : module type of BwdNoLabels.Notation
